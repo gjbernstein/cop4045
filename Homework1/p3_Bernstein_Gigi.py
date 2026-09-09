d45b51bd99d7564fb9cce5a65bc825c31daaf5ef
gigi <gbernstein2022@fau.edu>
2026-09-09 15:07:14 -0400

    complete

--- Homework1/p3_Bernstein_Gigi.py
+++ Homework1/p3_Bernstein_Gigi.py
@@ -1,7 +1,7 @@
 def find_dup_str(s:str, n):
     for i in range (len(s) - 2*n + 1): # only searches through the length where there could be a possible duplicate after
         substr = s[i: i + n] # substring of length n beginning with i
-        searchstr = s[n:] # the rest of the string after the substring
+        searchstr = s[i + n:] # the rest of the string after the substring
         if substr in searchstr:
             return substr
     return ""
@@ -11,12 +11,12 @@ n = int(input("enter length of duplicate to find: "))
 print(find_dup_str(s, n))
 
 def find_max_dup(s):
-    for n in reversed(range(2, len(s)//2)): # starting at the highest posiible length of duplicate
+    for n in reversed(range(2, len(s)//2 + 1)): # starting at the highest posiible length of duplicate
         found = find_dup_str(s, n)
         if found != "": #if a duplicate is found
             return found
     return found
 
 s = input("enter string: ")
-print(find_max_dup)
+print(find_max_dup(s))
 
